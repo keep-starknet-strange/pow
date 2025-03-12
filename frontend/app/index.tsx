067ef7fced7ff0a70b4d4d877a68fc154c2c969d
Zack Williams <zackwilldev@gmail.com>
2025-03-12 14:51:48 -0400

    adds mining comp

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { Text, View, ScrollView, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import BalanceDisplay from "./components/BalanceDisplay";
 import Block from "./components/Block";
 import Mempool from "./components/Mempool";
+import Mining from "./components/Mining";
 import "./global.css";
 
 export default function Index() {
@@ -89,30 +90,7 @@ export default function Index() {
       </View>
       )}
       {miningMode && (
-        <View className="flex-1 flex flex-col items-center mt-[30%]">
-          <View className="bg-[#f7f7f740] w-[80%] aspect-square rounded-xl border-2 border-[#f7f7f740] relative">
-            <View className="flex flex-wrap gap-1 w-full m-[0.4rem]">
-              {Array.from({ length: maxBlockTransactions }, (_, index) => (
-                <View
-                  key={index}
-                  className="bg-[#f7f7f7a0] rounded-xl w-[11%] aspect-square"
-                ></View>
-              ))}
-            </View>
-            <TouchableOpacity
-              className="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] bg-[#f7f7f7] rounded-full flex items-center justify-center
-                         border-2 border-[#17f717]"
-              onPress={tryMineBlock}
-            >
-              <Text className="text-[#171717] text-6xl m-4 mx-10">Mine!</Text>
-            </TouchableOpacity>
-          </View>
-          <Text className="text-[#f7f7f7] text-2xl mt-4">Difficulty {difficulty}</Text>
-          <Text className="text-[#f7f7f7] text-2xl">Nonce {nonce}</Text>
-          <Text className="text-[#f7f7f7] text-2xl">Hash {blockHash}</Text>
-          <Text className="text-[#f7f7f7] text-2xl mt-8">Reward {blockReward} BTC</Text>
-          <Text className="text-[#f7f7f7] text-2xl">Fees {blockFees.toFixed(2)} BTC</Text>
-        </View>
+        <Mining blockReward={blockReward} blockFees={blockFees} difficulty={difficulty} nonce={nonce} blockHash={blockHash} tryMineBlock={tryMineBlock} />
       )}
     </View>
   );
