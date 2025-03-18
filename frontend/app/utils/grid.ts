8e0175dd9aa2eb3c87921a374a39e16139860f30
Zack Williams <zackwilldev@gmail.com>
2025-03-18 14:54:15 -0400

    nm

--- frontend/app/utils/grid.ts
+++
@@ -1,4 +0,0 @@
-export const calculateWidthPercentage = (gridSize: number, gapSizePx: number, containerSizePx: number) => {
-  const totalGap = (gridSize - 1) * gapSizePx;
-  return ((containerSizePx - totalGap) / gridSize / containerSizePx) * 100;
-};
