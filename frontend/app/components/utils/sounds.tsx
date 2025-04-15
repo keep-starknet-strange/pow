ba6d6198f7c446e53e1e57686e8aa844e35d922e
Zack Williams <zackwilldev@gmail.com>
2025-04-15 13:39:12 -0400

    unused eslint disable

--- frontend/app/components/utils/sounds.tsx
+++ frontend/app/components/utils/sounds.tsx
@@ -1,7 +1,4 @@
 import { Audio } from "expo-av";
-import txClickedSource from "../../../assets/sounds/tx-clicked.mp3";
-import mineClickedSource from "../../../assets/sounds/mine-clicked.mp3";
-import blockMinedSource from "../../../assets/sounds/block-mined.mp3";
 
 export let soundEffectVolume = 1;
 export const setSoundEffectVolume = (volume: number) => {
@@ -20,6 +17,8 @@ export const playTxClicked = async (isSoundOn: boolean, pitch: number) => {
     staysActiveInBackground: false,
     shouldDuckAndroid: false,
   });
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const txClickedSource = require("../../../assets/sounds/tx-clicked.mp3");
   const { sound } = await Audio.Sound.createAsync({ uri: txClickedSource }, {
       rate: pitch + Math.random() *  0.1,
     });
@@ -28,7 +27,8 @@ export const playTxClicked = async (isSoundOn: boolean, pitch: number) => {
 
 export const playMineClicked = async (isSoundOn: boolean) => {
   if (!isSoundOn) return;
-  
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const mineClickedSource = require("../../../assets/sounds/mine-clicked.mp3");
   const { sound } = await Audio.Sound.createAsync({ uri: mineClickedSource }, {
       volume: 0.4,
       rate: 1.2
@@ -39,6 +39,8 @@ export const playMineClicked = async (isSoundOn: boolean) => {
 export const playBlockMined = async (isSoundOn: boolean) => {
   if (!isSoundOn) return;
 
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const blockMinedSource = require("../../../assets/sounds/block-mined.mp3");
   const { sound } = await Audio.Sound.createAsync({ uri: blockMinedSource }, {
       volume: 0.7,
       rate: 1.2
--- frontend/declarations.d.ts
+++ frontend/declarations.d.ts
@@ -1,11 +1,3 @@
-declare module "*.mp3" {
-  const src: string;
-  export default src;
-}
-declare module ".wav" {
-  const src: string;
-  export default src;
-}
 declare module "*.png" {
   const src: ImageSourcePropType;
   export default src;
