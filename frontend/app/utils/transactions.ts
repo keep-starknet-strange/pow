96e56fd3aec4e4879d7aea87665bafaa523a9631
Zack Williams <zackwilldev@gmail.com>
2025-04-15 14:11:14 -0400

    revert

--- frontend/app/utils/transactions.ts
+++ frontend/app/utils/transactions.ts
@@ -15,23 +15,23 @@ export const getRandomAddress = () =>
 export const getRandomFromArray = <T>(arr: T[]): T =>
   arr[Math.floor(Math.random() * arr.length)];
 
-const getRandomTransactionType = (isUpgradeActive: (id: number) 
-  => boolean) => {
+const getRandomTransactionType = (isUpgradeActive: (id: number
+) => boolean) => {
   const availableTypes = Object.entries(transactionTypesConfig)
   .filter(([_, { requiredUpgrade }]) => !requiredUpgrade || isUpgradeActive(requiredUpgrade)) // Only include if upgrade is active
   .map(([type, { chance }]) => ({ type, chance }));
-
+  
   // Normalizes probabilities to sum to 100%
   // TODO rework probabilities here
   const totalChance = availableTypes.reduce((sum, { chance }) => sum + chance, 0);
   let cumulativeChance = 0;
-  const rand = Math.random() * totalChance;  // Scale to total probability
-
+  const rand = Math.random() * totalChance; // Scale to total probability
+  
   for (const { type, chance } of availableTypes) {
     cumulativeChance += chance;
     if (rand < cumulativeChance) return type as TransactionType;
   }
-
+  
   return "Transfer"; // Default fallback
 };
 
