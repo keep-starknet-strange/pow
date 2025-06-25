81f1fc31e742af903f8263d1e51ba32f4504c817
Aleksefo <Aleksefo@gmail.com>
2025-06-25 14:27:56 +0200

    Replace navigation with React Navigation

---
+++ frontend/app/navigation/RootNavigator.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { View } from "react-native";
+
+import { LoginPage } from "../pages/LoginPage";
+import { TabNavigator } from "./TabNavigator";
+import { Header } from "../components/Header";
+import { InAppNotification } from "../components/InAppNotification";
+import { TutorialOverlay } from "../components/TutorialOverlay";
+
+import { useFocEngine } from "../context/FocEngineConnector";
+import { useTutorial } from "../context/Tutorial";
+
+const Stack = createStackNavigator();
+
+export function RootNavigator() {
+  const { user } = useFocEngine();
+  const { isTutorialActive } = useTutorial();
+
+  const isAuthenticated = user && user.account.username !== "";
+
+  return (
+    <View className="flex-1 bg-[#010a12ff] relative">
+      {isAuthenticated ? (
+        <>
+          {isTutorialActive && <TutorialOverlay />}
+          <Header />
+          <InAppNotification />
+          <TabNavigator />
+        </>
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginPage} />
+        </Stack.Navigator>
+      )}
+    </View>
+  );
+}
