13a99b88b0d41fa973c0a1a6d7d1dddb07f6984d
Michael Bakogiannis <micbakos@gmail.com>
2025-07-01 19:04:18 +0300

    Fix axe click on Android (#85)
    Co-authored-by: micbakos <michael.b@starkware.co>

--- frontend/app/components/PopupAnimation.tsx
+++ frontend/app/components/PopupAnimation.tsx
@@ -29,7 +29,7 @@ export const PopupAnimation: React.FC<PopupAnimationProps> = ({
 
   return (
     <Animated.View
-      className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center z-[3]"
+      className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center"
       style={{
         transform: [
           {
