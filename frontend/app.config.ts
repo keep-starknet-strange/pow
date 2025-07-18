3d52981ca59ecd5c2d2cf34c7e32f98ea7526312
micbakos <michael.b@starkware.co>
2025-07-18 10:23:17 +0300

    Update dependencies

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -39,6 +39,7 @@ const config: ExpoConfig = {
       favicon: "./assets/images/favicon.png"
     },
     plugins: [
+      "expo-web-browser",
       "expo-router",
       [
         "expo-splash-screen",
