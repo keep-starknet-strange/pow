22eaf3c614fc2a2576dd4b866232635c19542eca
Zack Williams <zackwilldev@gmail.com>
2025-08-07 15:11:26 -0400

    adds generateHash back

--- frontend/app/components/Miner.tsx
+++ frontend/app/components/Miner.tsx
@@ -9,7 +9,19 @@ interface MinerProps {
 }
 
 export const Miner: React.FC<MinerProps> = ({ triggerAnim, mineBlock }) => {
-  console.log("miningProgress");
+  const [mineHash, setMineHash] = useState("0xdEadBeefDeadbE");
+  const [mineColor, setMineColor] = useState("#CA1F4B");
+  const [mineStartTime, setMineStartTime] = useState(Date.now());
+  const generateRandomHash = (isDone: boolean) => {
+    const difficulty = 4; // TODO
+    const randomPart = Math.floor(Math.random() * 0xffffffffff)
+      .toString(14)
+      .padStart(14, "0");
+    // Replace first `difficulty` bytes with 00 if done
+    return isDone
+      ? `0x${"00".repeat(difficulty)}${randomPart}`
+      : `0x${randomPart}`;
+  };
   return (
     <View className="flex flex-col h-full aspect-square rounded-xl relative">
       <Confirmer
@@ -18,6 +30,11 @@ export const Miner: React.FC<MinerProps> = ({ triggerAnim, mineBlock }) => {
           mineBlock();
         }}
         renderedBy="miner"
+        confirmPopup={{
+          startTime: mineStartTime,
+          value: mineHash,
+          color: mineColor,
+        }}
       />
     </View>
   );
