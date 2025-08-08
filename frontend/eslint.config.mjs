f21dca007441893f682a646afa3fc159fdddb5a2
Zack Williams <zackwilldev@gmail.com>
2025-08-08 16:16:39 -0400

    adds compiler and compiler linter

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,17 +3,23 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  eslintPluginReactHooks.configs.recommended,
   {
+    plugins: {
+      "react-compiler": eslintPluginReactHooks,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-var-requires": 0,
       "@typescript-eslint/no-require-imports": "off",
+      'react-hooks/react-compiler': 'error',
     },
     ignores: [".expo/**"],
   },
