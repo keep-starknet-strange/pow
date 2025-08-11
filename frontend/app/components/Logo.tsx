07854a7d0207212f943e87c1406cd0ce93001539
Alex Fomushkin <aleksefo@gmail.com>
2025-08-11 18:22:44 +0200

    Animate logo in Login screen (#122)
    * Replace logo
    
    * Replace logo and adjust position
    
    * Replace logo with separate characters
    
    * Extract logo to a separate component
    
    * Add animated logo
    
    * Make sublogo animated as well
    
    * Convert images to webP
    
    * Fix the issue with Play not being pressable
    
    * Fix the issue with Play not being pressable
    
    * Replace logo
    
    * Replace logo and adjust position
    
    * Replace logo with separate characters
    
    * Extract logo to a separate component
    
    * Add animated logo
    
    * Make sublogo animated as well
    
    * Convert images to webP
    
    * Fix the issue with Play not being pressable
    
    * Fix the issue with Play not being pressable
    
    * Fix rebase issue
    
    * Fix logo issues
    
    * Fix logo issues

---
+++ frontend/app/components/Logo.tsx
@@ -0,0 +1,480 @@
+import React, { useEffect } from "react";
+import { View, Dimensions } from "react-native";
+import {
+  Canvas,
+  Image,
+  FilterMode,
+  MipmapMode,
+  Group,
+  ColorMatrix,
+} from "@shopify/react-native-skia";
+import { useImages } from "../hooks/useImages";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  withDelay,
+  useDerivedValue,
+} from "react-native-reanimated";
+
+export const Logo: React.FC = () => {
+  const { getImage } = useImages();
+  const { width: screenWidth } = Dimensions.get("window");
+
+  // Scale factor based on screen width (iPhone SE width is 375)
+  const scaleFactor = Math.min(screenWidth / 400, 1);
+
+  // Animated values for character Y positions (falling from top)
+  const pShadowY = useSharedValue(-200);
+  const oShadowY = useSharedValue(-250);
+  const wShadowY = useSharedValue(-300);
+  const exclamationShadowY = useSharedValue(-350);
+
+  const pMainY = useSharedValue(-200);
+  const oMainY = useSharedValue(-250);
+  const wMainY = useSharedValue(-300);
+  const exclamationMainY = useSharedValue(-350);
+
+  const sublogoX = useSharedValue(400);
+
+  // Fixed X positions for each character (scaled)
+  const pX = 30 * scaleFactor;
+  const oX = 104 * scaleFactor;
+  const wX = 181 * scaleFactor;
+  const exclamationX = 310 * scaleFactor;
+
+  const pShadowX = 42 * scaleFactor;
+  const oShadowX = 116 * scaleFactor;
+  const wShadowX = 193 * scaleFactor;
+  const exclamationShadowX = 322 * scaleFactor;
+
+  // Derived values for white outlines (offset from main letters)
+  const pOutlineXDerived = pX - 2 * scaleFactor;
+  const oOutlineXDerived = oX - 2 * scaleFactor;
+  const wOutlineXDerived = wX - 2 * scaleFactor;
+  const exclamationOutlineXDerived = exclamationX - 2 * scaleFactor;
+
+  const pShadowOutlineXDerived = pShadowX - 2 * scaleFactor;
+  const oShadowOutlineXDerived = oShadowX - 2 * scaleFactor;
+  const wShadowOutlineXDerived = wShadowX - 2 * scaleFactor;
+  const exclamationShadowOutlineXDerived = exclamationShadowX - 2 * scaleFactor;
+
+  // Derived values for Y positions with outline offsets
+  const pShadowOutlineYDerived = useDerivedValue(
+    () => pShadowY.value - 1 * scaleFactor,
+  );
+  const oShadowOutlineYDerived = useDerivedValue(
+    () => oShadowY.value - 1 * scaleFactor,
+  );
+  const wShadowOutlineYDerived = useDerivedValue(
+    () => wShadowY.value - 1 * scaleFactor,
+  );
+  const exclamationShadowOutlineYDerived = useDerivedValue(
+    () => exclamationShadowY.value - 1 * scaleFactor,
+  );
+
+  const pOutlineYDerived = useDerivedValue(
+    () => pMainY.value - 2 * scaleFactor,
+  );
+  const oOutlineYDerived = useDerivedValue(
+    () => oMainY.value - 2 * scaleFactor,
+  );
+  const wOutlineYDerived = useDerivedValue(
+    () => wMainY.value - 2 * scaleFactor,
+  );
+  const exclamationOutlineYDerived = useDerivedValue(
+    () => exclamationMainY.value - 2 * scaleFactor,
+  );
+
+  useEffect(() => {
+    // Animate characters falling from top with bouncy spring animation
+    // Final positions adjusted for canvas offset (200px down due to marginTop: -200)
+    const finalY = 232 * scaleFactor;
+    const mainFinalY = 220 * scaleFactor;
+
+    pShadowY.value = withDelay(
+      100,
+      withSpring(finalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+    pMainY.value = withDelay(
+      100,
+      withSpring(mainFinalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+
+    oShadowY.value = withDelay(
+      200,
+      withSpring(finalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+    oMainY.value = withDelay(
+      200,
+      withSpring(mainFinalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+
+    wShadowY.value = withDelay(
+      300,
+      withSpring(finalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+    wMainY.value = withDelay(
+      300,
+      withSpring(mainFinalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+
+    exclamationShadowY.value = withDelay(
+      400,
+      withSpring(finalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+    exclamationMainY.value = withDelay(
+      400,
+      withSpring(mainFinalY, {
+        damping: 10,
+        stiffness: 100,
+        mass: 1,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+
+    sublogoX.value = withDelay(
+      600,
+      withSpring(100 * scaleFactor, {
+        damping: 12,
+        stiffness: 90,
+        mass: 0.8,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+      }),
+    );
+  }, []);
+
+  return (
+    <Animated.View
+      className="absolute"
+      style={{
+        width: "90%",
+        maxWidth: 360 * scaleFactor,
+        height: 600 * scaleFactor,
+        top: "30%",
+        marginTop: -200 * scaleFactor,
+        overflow: "visible",
+        zIndex: 10,
+        pointerEvents: "none",
+      }}
+    >
+      <Canvas
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: 600 * scaleFactor,
+          overflow: "visible",
+        }}
+      >
+        {/* White outlines for shadows */}
+        <Group>
+          <ColorMatrix
+            matrix={[
+              0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+            ]}
+          />
+          <Image
+            image={getImage("logoExclamation")}
+            fit="contain"
+            x={exclamationShadowOutlineXDerived}
+            y={exclamationShadowOutlineYDerived}
+            width={34 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoW")}
+            fit="contain"
+            x={wShadowOutlineXDerived}
+            y={wShadowOutlineYDerived}
+            width={140 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoO")}
+            fit="contain"
+            x={oShadowOutlineXDerived}
+            y={oShadowOutlineYDerived}
+            width={90 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoP")}
+            fit="contain"
+            x={pShadowOutlineXDerived}
+            y={pShadowOutlineYDerived}
+            width={86 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+        </Group>
+
+        {/* White outlines for main letters */}
+        <Group>
+          <ColorMatrix
+            matrix={[
+              0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+            ]}
+          />
+          <Image
+            image={getImage("logoExclamation")}
+            fit="contain"
+            x={exclamationOutlineXDerived}
+            y={exclamationOutlineYDerived}
+            width={34 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoW")}
+            fit="contain"
+            x={wOutlineXDerived}
+            y={wOutlineYDerived}
+            width={140 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoO")}
+            fit="contain"
+            x={oOutlineXDerived}
+            y={oOutlineYDerived}
+            width={90 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoP")}
+            fit="contain"
+            x={pOutlineXDerived}
+            y={pOutlineYDerived}
+            width={86 * scaleFactor}
+            height={84 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+        </Group>
+
+        {/* Purple shadows */}
+        <Group>
+          <ColorMatrix
+            matrix={[
+              0, 0, 0.263, 0, 0.263, 0, 0, 0.051, 0, 0.051, 0, 0, 0.569, 0,
+              0.569, 0, 0, 0, 1, 0,
+            ]}
+          />
+          <Image
+            image={getImage("logoExclamation")}
+            fit="contain"
+            x={exclamationShadowX}
+            y={exclamationShadowY}
+            width={30 * scaleFactor}
+            height={80 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoW")}
+            fit="contain"
+            x={wShadowX}
+            y={wShadowY}
+            width={136 * scaleFactor}
+            height={80 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoO")}
+            fit="contain"
+            x={oShadowX}
+            y={oShadowY}
+            width={86 * scaleFactor}
+            height={80 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+          <Image
+            image={getImage("logoP")}
+            fit="contain"
+            x={pShadowX}
+            y={pShadowY}
+            width={82 * scaleFactor}
+            height={80 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+        </Group>
+
+        {/* Main letters */}
+        <Image
+          image={getImage("logoExclamation")}
+          fit="contain"
+          x={exclamationX}
+          y={exclamationMainY}
+          width={30}
+          height={80}
+          sampling={{
+            filter: FilterMode.Nearest,
+            mipmap: MipmapMode.Nearest,
+          }}
+        />
+        <Image
+          image={getImage("logoW")}
+          fit="contain"
+          x={wX}
+          y={wMainY}
+          width={136}
+          height={80}
+          sampling={{
+            filter: FilterMode.Nearest,
+            mipmap: MipmapMode.Nearest,
+          }}
+        />
+        <Image
+          image={getImage("logoO")}
+          fit="contain"
+          x={oX}
+          y={oMainY}
+          width={86}
+          height={80}
+          sampling={{
+            filter: FilterMode.Nearest,
+            mipmap: MipmapMode.Nearest,
+          }}
+        />
+        <Image
+          image={getImage("logoP")}
+          fit="contain"
+          x={pX}
+          y={pMainY}
+          width={82}
+          height={80}
+          sampling={{
+            filter: FilterMode.Nearest,
+            mipmap: MipmapMode.Nearest,
+          }}
+        />
+      </Canvas>
+      <View
+        className="absolute"
+        style={{
+          width: 600 * scaleFactor,
+          height: 18 * scaleFactor,
+          top: 320 * scaleFactor,
+          right: (30 - 300) * scaleFactor,
+          zIndex: -1,
+          overflow: "visible",
+          paddingLeft: 100 * scaleFactor,
+        }}
+      >
+        <Canvas
+          style={{
+            position: "absolute",
+            left: 0,
+            width: 600 * scaleFactor,
+            height: "100%",
+            overflow: "visible",
+          }}
+        >
+          <Image
+            image={getImage("sublogo")}
+            fit="contain"
+            x={sublogoX}
+            y={0}
+            width={182 * scaleFactor}
+            height={18 * scaleFactor}
+            sampling={{
+              filter: FilterMode.Nearest,
+              mipmap: MipmapMode.Nearest,
+            }}
+          />
+        </Canvas>
+      </View>
+    </Animated.View>
+  );
+};
