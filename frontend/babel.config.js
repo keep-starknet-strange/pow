ade7d7b541e79a1cb583e0e84764cc5f72dd4dd1
Alex Fomushkin <aleksefo@gmail.com>
2025-11-25 18:04:13 +0100

    Remove console logs in prod to improve performance (#233)

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -6,8 +6,11 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      "react-native-reanimated/plugin"
-    ]
+    plugins: ["react-native-reanimated/plugin"],
+    env: {
+      production: {
+        plugins: ["transform-remove-console"],
+      },
+    },
   };
 };
